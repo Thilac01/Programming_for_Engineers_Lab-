d9ed1bbed029df214d2110b8a85d999717820b9b
Thilac <163951647+Thilac01@users.noreply.github.com>
2024-09-19 11:51:39 +0530

    Update Parking.py

--- LAB6/Parking.py
+++ LAB6/Parking.py
@@ -1,20 +1,22 @@
 class ParkingSpot:
-
-    def __init__ (self,spot_num,available,vechile_num):
+    
+    def __init__ (self, spot_num, available, vehicle_num):
         self.spot_num = spot_num
         self.available = available
-        self.vechile_num = vechile_num
+        self.vehicle_num = vehicle_num
 
     def get_info(self):
-        print(self.spot_num , self.available , self.vechile_num)
+        if self.available:
+            print(f"Spot {self.spot_num}: Available")
+        else:
+            print(f"Spot {self.spot_num}: Occupied by {self.vehicle_num}")
 
-
-    def update_availability(self,status,vechile_num):
+    def update_availability(self, status, vehicle_num):
         self.available = status
-        self.vechile_num = vechile_num
+        self.vehicle_num = vehicle_num
 
 if __name__ == '__main__':
-    parking = ParkingSpot(15,True,None)
-    parking.get_info()
-    parking.update_availability(False,'QBQ -4533')
-    parking.get_info()
+    parking = ParkingSpot(15, True, None)
+    parking.get_info()  
+    parking.update_availability(False, 'QBQ -4533')
+    parking.get_info()  
